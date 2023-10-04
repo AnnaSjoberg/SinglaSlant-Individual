84d0a0707d3b3fcaf408d1e6888cd736c2fe9d66
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-10-04 17:42:55 +0200

    test of index endpoint is fine

---
+++ src/test/java/com/example/singlaslantindividual/controllers/CoinFlipControllerAPITest.java
@@ -0,0 +1,57 @@
+package com.example.singlaslantindividual.controllers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.example.singlaslantindividual.services.Game;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CoinFlipControllerAPITest {
+
+    @MockBean
+    private Game game;
+    @Autowired
+    private CoinFlipController controller;
+
+    @BeforeEach
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(controller);
+    }
+
+
+    @Test
+    public void indexEndpointShouldContainStartingPointStatsOnOk() {
+        given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .body(containsString("User Score:"))
+                .body(containsString("Computer Score:"))
+                .body(containsString("Turns:"))
+                .body(containsString("Win Rate:"));
+    }
+
+    @Test
+    public void publishFlipEndpointShouldContainWinnerOnOk() {
+        given()
+                .param("choice", "heads") // Set the request parameter as needed
+                .when()
+                .post("/flip")
+                .then()
+                .statusCode(200)
+                .body("WINNER", containsString("WINS!!!!!"));
+    }
+}
