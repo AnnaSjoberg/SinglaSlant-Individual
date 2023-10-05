901765a65a58fbcae6c83c74d04395548a98b6f1
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-10-05 12:24:43 +0200

    null check for roundResult

--- src/main/java/com/example/singlaslantindividual/controllers/CoinFlipController.java
+++ src/main/java/com/example/singlaslantindividual/controllers/CoinFlipController.java
@@ -6,8 +6,10 @@ import com.example.singlaslantindividual.model.RoundResult;
 import com.example.singlaslantindividual.services.Game;
 import com.example.singlaslantindividual.services.RandomNumberGenerator;
 import com.example.singlaslantindividual.services.WinRateCalculator;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -58,7 +60,7 @@ public class CoinFlipController {
 
     //När användaren valt heads/tails uppdateras sidan med relevant information i /flip-endpointen
     //Användaren fortsätter spela så många omgångar han/hon vill
-    @PostMapping("/flip")
+/*    @PostMapping("/flip")
     public String publishFlip(@RequestParam String choice, Model model) {
         double resultAsDouble = randomNumberGenerator.generateRandomNumber();
         RoundResult roundResult = game.playGame(choice, resultAsDouble);
@@ -67,6 +69,29 @@ public class CoinFlipController {
         rounds.add(roundResult);
         model.addAttribute("rounds", rounds);
         model.addAttribute("Result", roundResult.getWinner() + " WINS!!!!!");
+        model.addAttribute("userScore", game.getCoinFlip().getUserScore());
+        model.addAttribute("computerScore", game.getCoinFlip().getComputerScore());
+        model.addAttribute("turns", game.getCoinFlip().getTurns());
+        model.addAttribute("winRate", winRate);
+
+        return "index";
+    }
+
+ */
+    @PostMapping("/flip")
+    public String publishFlip(@RequestParam String choice, Model model) {
+        double resultAsDouble = randomNumberGenerator.generateRandomNumber();
+        RoundResult roundResult = game.playGame(choice, resultAsDouble);
+        double winRate = winRateCalculator.calculateWinRate(game.getCoinFlip());
+        String result = "";
+        if (roundResult == null) {
+            result = choice + " is not a valid choice. Please try again!";
+        } else {
+            result = roundResult.getWinner() + " WINS!!!!!";
+            rounds.add(roundResult);
+        }
+        model.addAttribute("rounds", rounds);
+        model.addAttribute("Result", result);
         model.addAttribute("userScore", game.getCoinFlip().getUserScore());
         model.addAttribute("computerScore", game.getCoinFlip().getComputerScore());
         model.addAttribute("turns", game.getCoinFlip().getTurns());
